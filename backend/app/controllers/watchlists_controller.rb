8d2e7698aa2bf0f557f59a465d93aad574b2fa20
dianawhalen <whalendianaj@gmail.com>
2023-06-20 05:22:41 -0600

    Added watchlists controller actions and before_action

--- backend/app/controllers/watchlists_controller.rb
+++ backend/app/controllers/watchlists_controller.rb
@@ -1,11 +1,44 @@
 class WatchlistsController < ApplicationController
+  before_action :set_watchlist, only: [:show, :update, :destroy]
+
   def index
-    watchlists = User.all
+    watchlists = Watchlist.all
     render json: watchlists
   end
 
   def show
-    watchlist = User.find(params[:id])
-    render json: watchlist
+    render json: @watchlist
+  end
+
+  def create
+    watchlist = Watchlist.new(user: User.find(params[:user_id]))
+
+    if watchlist.save
+      render json: watchlist, status: :created
+    else
+      render json: watchlist.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @watchlist.update(watchlist_params)
+      render json: @watchlist
+    else
+      render json: @watchlist.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @watchlist.destroy
+  end
+
+  private
+
+  def set_watchlist
+    @watchlist = Watchlist.find(params[:id])
+  end
+
+  def watchlist_params
+    params.permit(:user_id)
   end
 end
