9a05707930ab731bbe7e553e640a0f76a1c3ac2e
dianawhalen <whalendianaj@gmail.com>
2023-06-20 05:22:12 -0600

    Added releases controller actions and before_action

--- backend/app/controllers/releases_controller.rb
+++ backend/app/controllers/releases_controller.rb
@@ -1,11 +1,44 @@
 class ReleasesController < ApplicationController
+  before_action :set_release, only: [:show, :update, :destroy]
+
   def index
-    releases = User.all
+    releases = Release.all
     render json: releases
   end
 
   def show
-    release = User.find(params[:id])
-    render json: release
+    render json: @release
+  end
+
+  def create
+    release = Release.new(release_params)
+
+    if release.save
+      render json: release, status: :created
+    else
+      render json: release.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @release.update(release_params)
+      render json: @release
+    else
+      render json: @release.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @release.destroy
+  end
+
+  private
+
+  def set_release
+    @release = Release.find(params[:id])
+  end
+
+  def release_params
+    params.require(:release).permit(:title, :type, :tmdb_id, :tmdb_type, :imdb_id, :season_number, :poster_url, :source_release_date, :source_id, :source_name, :is_original, :watchlist_id)
   end
 end
