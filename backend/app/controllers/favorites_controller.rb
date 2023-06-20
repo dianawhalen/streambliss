1988fe4d0ee1ba72a927e213843ce87505c38828
dianawhalen <whalendianaj@gmail.com>
2023-06-20 05:23:58 -0600

    Added favorites controller actions and before_action

--- backend/app/controllers/favorites_controller.rb
+++ backend/app/controllers/favorites_controller.rb
@@ -1,11 +1,44 @@
 class FavoritesController < ApplicationController
+  before_action :set_favorite, only: [:show, :update, :destroy]
+
   def index
-    favorites = User.all
+    favorites = Favorite.all
     render json: favorites
   end
 
   def show
-    favorite = User.find(params[:id])
-    render json: favorite
+    render json: @favorite
+  end
+
+  def create
+    favorite = Favorite.new(favorite_params)
+
+    if favorite.save
+      render json: favorite, status: :created
+    else
+      render json: favorite.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @favorite.update(favorite_params)
+      render json: @favorite
+    else
+      render json: @favorite.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @favorite.destroy
+  end
+
+  private
+
+  def set_favorite
+    @favorite = Favorite.find(params[:id])
+  end
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :release_id)
   end
 end
