97d39839a0737316fd56cb92c58d09ae8039c977
dianawhalen <whalendianaj@gmail.com>
2023-06-25 03:04:13 -0600

    Updated the routes to include new models

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :create, :update, :destroy]
-  resources :watchlists, only: [:show, :create, :update, :destroy]
-  resources :releases, only: [:index, :show]
-  resources :favorites, only: [:create, :index, :destroy]
+  resources :users, only: [:show, :create, :update, :destroy] do
+    resource :watchlist, only: [:show, :update]
+    resources :favorites, only: [:index, :update]
+  end
+
+  resources :releases, only: [:index, :show] do
+    resources :sources, only: [:index]
+  end
+
+  resources :sources, only: [:index] do
+    resources :releases, only: [:index]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
