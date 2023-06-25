69f2df4e382ce7af3eb4a4c17b2e7221a2ac46e8
dianawhalen <whalendianaj@gmail.com>
2023-06-25 03:25:50 -0600

    Setup SessionsController and added views for user login, user show, and user creation

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url
   end
 end
