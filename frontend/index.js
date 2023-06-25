4beefaded7e6ed5199c96ac10ea9d48e1e656456
dianawhalen <whalendianaj@gmail.com>
2023-06-25 04:54:13 -0600

    Added sessions#new functionality to frontend JavaScript

--- frontend/index.js
+++ frontend/index.js
@@ -53,3 +53,41 @@ document.querySelector('#show-user-link').addEventListener('click', function() {
   const id = this.dataset.userId;
   fetchUser(id);
 });
+
+function newSessionForm() {
+  return `
+    <form id="new-session-form">
+      <label for="email">Email:</label><br>
+      <input type="text" id="email" name="email"><br>
+      <label for="password">Password:</label><br>
+      <input type="password" id="password" name="password"><br>
+      <input type="submit" value="Submit">
+    </form>
+  `;
+}
+
+function displayNewSessionForm() {
+  const main = document.querySelector('main');
+  main.innerHTML = newSessionForm();
+}
+
+document.querySelector('#new-session-link').addEventListener('click', displayNewSessionForm);
+
+document.querySelector('main').addEventListener('submit', function(event) {
+  if (event.target.matches('#new-session-form')) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(Object.fromEntries(formData))
+    }).then(response => response.json())
+      .then(user => {
+        localStorage.setItem('currentUserId', user.id);
+        displayUser(user);
+      });
+  }
+});
