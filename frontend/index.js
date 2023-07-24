9dc3d10154850a27917a00563d09b2d628682868
dianawhalen <whalendianaj@gmail.com>
2023-07-24 14:19:56 -0600

    Updated front-end fetch requests and display to include release_type.

--- frontend/index.js
+++ frontend/index.js
@@ -44,6 +44,7 @@ function displayUser(user) {
     <h1>${user.username}</h1>
     <p>${user.email}</p>
   `;
+  fetchData();
 }
 
 function fetchUser(id) {
@@ -101,33 +102,70 @@ document.querySelector('main').addEventListener('submit', function(event) {
   }
 });
 
-function displayNewSessionForm() {
-  const main = document.querySelector('main');
-  const form = document.createElement('div');
-  form.innerHTML = newSessionForm();
-  main.appendChild(form);
+document.addEventListener('DOMContentLoaded', function() {
+const currentUserId = localStorage.getItem('currentUserId');
+if (currentUserId) {
+  fetchUser(currentUserId);
+} else {
+  displayNewSessionForm();
+}
+});
+
+function postRelease(release) {
+  fetch('http://localhost:3000/releases', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      release: {
+        title: release.title,
+        release_type: release.type,
+        show_id: release.show_id,
+        year: release.year,
+        result_type: release.result_type,
+        poster_url: release.poster_url
+      }
+    })
+  })
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch((error) => console.error('Error:', error));
+}
+
+
+function displayRelease(release) {
+const main = document.querySelector('main');
+const div = document.createElement('div');
+div.innerHTML = `
+  <h2>${release.title}</h2>
+  <p>Type: ${release.type}</p>
+  <p>Year: ${release.year}</p>
+  <img src="${release.poster_url}" alt="${release.title}">
+`;
+main.appendChild(div);
 }
 
 function fetchData() {
   fetch('https://api.watchmode.com/v1/autocomplete-search/?apiKey=wXr0tpIrPU0QoupBDhPmvzeKtYSX97Dxdd4nekY3&search_value=Breaking%20bad&search_type=1')
     .then(response => response.json())
     .then(data => {
-      const mappedData = data.map(item => ({
+      console.log("Data from API:", data);
+
+      const mappedData = data.results.map(item => ({
         title: item.name,
-        type: item.type,
+        release_type: item.type,
         show_id: item.id,
         year: item.year,
         result_type: item.result_type,
         poster_url: item.image_url
       }));
 
-      const main = document.querySelector('main');
-      const p = document.createElement('p');
-      p.textContent = JSON.stringify(mappedData);
-      main.appendChild(p);
+      mappedData.forEach(release => {
+        displayRelease(release);
+        postRelease(release);
+      });
     })
     .catch(error => console.error('Error:', error));
 }
-
-displayNewSessionForm();
-fetchData();
